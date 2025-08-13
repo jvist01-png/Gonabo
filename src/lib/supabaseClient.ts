e4c188b3f1e0a8b18b265cc50b331b28d183a28e
Jonas <jvist01@gmail.com>
2025-08-13 22:04:00 +0200

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,5 @@
+'use client';
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 
-export const supabase = createPagesBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+// v0.10.x expects no arguments
+export const supabase = createPagesBrowserClient();
